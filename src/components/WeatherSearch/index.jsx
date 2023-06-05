ad174aa88146ab619caf8468d7900fb41ed64c1d
KomarovVladimir <komarov2142@gmail.com>
2023-06-05 16:29:40 +0300

    Code split

--- src/components/WeatherSearch/index.jsx
+++ src/components/WeatherSearch/index.jsx
@@ -1,36 +1,19 @@
 import React from "react";
-import { useState } from "react";
 
 import { Paper, InputBase, Divider, IconButton } from "@mui/material";
 
 import LocationOnIcon from "@mui/icons-material/LocationOn";
 import SearchIcon from "@mui/icons-material/Search";
+import { useWeatherSearch } from "./useWeatherSearch";
 
 export const WeatherSearch = ({ setQuery, units, setUnits }) => {
-    const [city, setCity] = useState("");
-
-    const handleUnitsChange = (e) => {
-        const selectedUnit = e.currentTarget.name;
-        if (units !== selectedUnit) setUnits(selectedUnit);
-    };
-
-    const handleSearchClick = (e) => {
-        e.preventDefault();
-        if (city !== "") setQuery({ q: city });
-    };
-
-    const handleLocationClick = () => {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition((position) => {
-                let lat = position.coords.latitude;
-                let lon = position.coords.longitude;
-                setQuery({
-                    lat,
-                    lon,
-                });
-            });
-        }
-    };
+    const {
+        city,
+        setCity,
+        handleUnitsChange,
+        handleSearchClick,
+        handleLocationClick,
+    } = useWeatherSearch({ setQuery, units, setUnits });
 
     return (
         <Paper
