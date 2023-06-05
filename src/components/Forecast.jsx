10a3e752499598ce9141c5e06027f842126f42f3
KomarovVladimir <komarov2142@gmail.com>
2023-06-05 15:53:12 +0300

    Remove the extra return statements

--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -2,38 +2,31 @@ import React from "react";
 import { Box, Stack, Typography } from "@mui/material";
 import { getIconUrl } from "../services/utils";
 
-export const Forecast = ({ title, items }) => {
-    return (
-        <Box sx={{ mt: 8 }}>
-            <Typography variant="h6" align="center" color="primary">
-                {title}
-            </Typography>
-            <Stack
-                sx={{ mt: 4 }}
-                direction="row"
-                justifyContent="center"
-                textAlign="center"
-                gap={4}
-                useFlexGap
-                flexWrap="wrap"
-            >
-                {items.map((item, index) => (
-                    <Stack key={index} gap={1}>
-                        <Typography color="text.secondary">
-                            {item.title}
-                        </Typography>
-                        <Box>
-                            <img
-                                src={getIconUrl(item.icon)}
-                                alt="Weather Icon"
-                            />
-                        </Box>
-                        <Typography color="text.secondary">
-                            {item.temp.toFixed()}°
-                        </Typography>
-                    </Stack>
-                ))}
-            </Stack>
-        </Box>
-    );
-};
+export const Forecast = ({ title, items }) => (
+    <Box sx={{ mt: 8 }}>
+        <Typography variant="h6" align="center" color="primary">
+            {title}
+        </Typography>
+        <Stack
+            sx={{ mt: 4 }}
+            direction="row"
+            justifyContent="center"
+            textAlign="center"
+            gap={4}
+            useFlexGap
+            flexWrap="wrap"
+        >
+            {items.map((item, index) => (
+                <Stack key={index} gap={1}>
+                    <Typography color="text.secondary">{item.title}</Typography>
+                    <Box>
+                        <img src={getIconUrl(item.icon)} alt="Weather Icon" />
+                    </Box>
+                    <Typography color="text.secondary">
+                        {item.temp.toFixed()}°
+                    </Typography>
+                </Stack>
+            ))}
+        </Stack>
+    </Box>
+);
--- src/components/TimeLocation.jsx
+++ src/components/TimeLocation.jsx
@@ -2,25 +2,23 @@ import React from "react";
 import { formatToLocalTime } from "../services/utils";
 import { Typography, Box } from "@mui/material";
 
-export const TimeLocation = ({ weather: { dt, timezone, name, country } }) => {
-    return (
-        <Box sx={{ mt: 6 }}>
-            <Typography
-                component="h1"
-                variant="h2"
-                align="center"
-                color="text.primary"
-            >
-                {name}, {country}
-            </Typography>
-            <Typography
-                variant="h5"
-                align="center"
-                color="text.secondary"
-                paragraph
-            >
-                {formatToLocalTime(dt, timezone)}
-            </Typography>
-        </Box>
-    );
-};
+export const TimeLocation = ({ weather: { dt, timezone, name, country } }) => (
+    <Box sx={{ mt: 6 }}>
+        <Typography
+            component="h1"
+            variant="h2"
+            align="center"
+            color="text.primary"
+        >
+            {name}, {country}
+        </Typography>
+        <Typography
+            variant="h5"
+            align="center"
+            color="text.secondary"
+            paragraph
+        >
+            {formatToLocalTime(dt, timezone)}
+        </Typography>
+    </Box>
+);
